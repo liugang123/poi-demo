9c05d2d2a1972b58daf9c55d6156e2c1a91b4aaa
liugang19 <liugang19@xdf.cn>
2021-02-07 17:23:24 +0800

    fix:  create project 0207

---
+++ src/main/java/com/example/poidemo/word/CreateWordDocument.java
@@ -0,0 +1,30 @@
+package com.example.poidemo.word;
+
+import org.apache.poi.xwpf.usermodel.XWPFDocument;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+/**
+ * 1.创建word文档
+ *
+ * @author liugang
+ * @create 2021/2/7
+ */
+public class CreateWordDocument {
+
+    public static void main(String[] args) throws IOException {
+
+        // 1.创建空文件
+        XWPFDocument document = new XWPFDocument();
+
+        // 2.文件输出
+        FileOutputStream out = new FileOutputStream(new File("first-word.docx"));
+        document.write(out);
+
+        out.close();
+
+        System.out.println("Create First Word Document");
+    }
+}
